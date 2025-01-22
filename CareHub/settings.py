731033c32c73a1be51754a09e15577f9c8345c9d
IsratJahanR <isratjahan151q@gmail.com>
2025-01-23 01:26:58 +0600

    secure private key

--- CareHub/settings.py
+++ CareHub/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import os
+from django.core.exceptions import ImproperlyConfigured 
+from decouple import config
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
@@ -11,7 +15,11 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-key-for-dev')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+
+if not SECRET_KEY:
+    raise ImproperlyConfigured("The SECRET_KEY environment variable is not set.")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
