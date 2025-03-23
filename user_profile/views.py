5ec79a4a32af6b77a3c972603ddaa8c4858a29af
asfak27 <shahrierasfak27@gmail.com>
2025-03-23 23:03:38 +0600

    fix: fixed myprofile link up issue

--- user_profile/views.py
+++ user_profile/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseNotAllowed
 
 def profile(request):
     return render(request,'app/customer_profile/my-profile.html')
+
 def address(request):
     return render(request,'app/customer_profile/address.html')
 
