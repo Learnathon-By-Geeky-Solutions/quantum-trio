998beb97b3441006807b593c13029516e52a3d50
IsratJahanR <isratjahan151q@gmail.com>
2025-03-29 20:20:47 +0600

    complete booking

--- booking/urls.py
+++ booking/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.booking_step_1, name='index'),
     path('booking-step-2', views.booking_step_2, name='step-2'), 
     path('available_slots/', views.available_slots, name='available_slots'),
+    path('success', views.success, name='success'),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
