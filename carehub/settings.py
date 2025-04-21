f8433f62d527d4e66e2ad197b7bdfd6024b07c3f
rakib151p <rakib151p@gmail.com>
2025-04-22 03:18:04 +0600

    chore: comented allowed host temporary

--- carehub/settings.py
+++ carehub/settings.py
@@ -24,7 +24,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
