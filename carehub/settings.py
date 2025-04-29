e60d83b3648e987c4eb614e53c733016569dc3b6
rakib151p <rakib151p@gmail.com>
2025-04-30 01:44:43 +0600

    fix: csrf variable added

--- carehub/settings.py
+++ carehub/settings.py
@@ -26,7 +26,7 @@ if not SECRET_KEY:
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",")
-# CSRF_TRUSTED_ORIGINS=['https://carehub.up.railway.app']
+CSRF_TRUSTED_ORIGINS=['https://carehub.up.railway.app']
 
 # Application definition
 
